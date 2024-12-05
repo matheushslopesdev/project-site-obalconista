ab5369f0ea5b942bde91607d289fd4eff0d49f03
Matheus Lopes <matheushslopes.developer@gmail.com>
2024-12-04 21:35:28 -0300

    update

--- public/js/validadeReceita.js
+++ public/js/validadeReceita.js
@@ -2,19 +2,37 @@
 const data = document.querySelector('#data')
 const btn = document.querySelector('#btn-calc')
 const btnSelect = document.querySelector('#tipo-receita')
+const resultado = document.querySelector('#resultado')
 
 
 btn.addEventListener('click', (e)=>{
-    if(btnSelect.value === 'popular'){
-        const dateNew = Date.now()
-        
-        console.log(dateNew)
 
+    console.log(isNaN(data.value))
+    if(!isNaN(data.value)){
+        return resultado.innerHTML = `Data Invalida`
     }
-    if(btnSelect.value === 'antimicrobiando'){
-        
+    if(btnSelect.value === 'popular'){
+        calcDate(180,data)
+    }
+    if(btnSelect.value === 'antimicrobiano'){
+        calcDate(10,data)
     }
     if(btnSelect.value === 'receitacomum'){
-        
+        calcDate(30,data)
     }
 })
+
+
+
+function calcDate(dias,date){
+    const dateNow = Date.now().toFixed(1)
+        const dataInput = new Date(date.value).getTime()
+        let calc = dateNow - dataInput 
+        let dia = (calc / 3600000) / 24
+
+        if(dia.toFixed(0) < dias){
+    
+            return resultado.innerHTML = `Receituario ainda tem Validade - ${dia.toFixed(0) -1} Dias`
+        }
+        return resultado.innerHTML = `Receituario Sem Validade - ${dia.toFixed(0) -1} Dias`
+}
